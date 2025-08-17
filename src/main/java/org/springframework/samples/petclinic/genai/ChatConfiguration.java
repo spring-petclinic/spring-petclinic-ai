511ff56628cdc669af2cf6500c00812532be6c18
Antoine Rey <antoine.rey@free.fr>
2025-08-17 17:37:22 +0200

    Update Spring AI to 1.0.1

--- src/main/java/org/springframework/samples/petclinic/genai/ChatConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/genai/ChatConfiguration.java
@@ -20,13 +20,10 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-
-import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.DEFAULT_CHAT_MEMORY_CONVERSATION_ID;
 
 @Configuration
 class ChatConfiguration {
@@ -40,18 +37,13 @@ class ChatConfiguration {
 		return chatClientBuilder
 			.defaultAdvisors(
 				// Chat memory helps us keep context when using the chatbot for up to 10 previous messages.
-				new MessageChatMemoryAdvisor(chatMemory, DEFAULT_CHAT_MEMORY_CONVERSATION_ID, 10), // CHAT MEMORY
+				MessageChatMemoryAdvisor.builder(chatMemory).build(),
 				new SimpleLoggerAdvisor()
 			)
 			.defaultSystem(systemResource)
 			.defaultTools(petclinicTools)
 			.build();
 		// @formatter:on
-	}
-
-	@Bean
-	public ChatMemory chatMemory() {
-		return new InMemoryChatMemory();
 	}
 
 }
